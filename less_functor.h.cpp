58c11c4f4ca8c76c7747b19e0044da13d552af7c
patriciafigueroa <patricia.figueroa@ucdenver.edu>
2016-05-06 21:23:50 -0600

    less_functor.h

---
+++ less_functor.h.cpp
@@ -0,0 +1,44 @@
+//
+//  less_functor.h
+//  PA5
+//
+//  Created by Patricia Figueroa on 5/4/16.
+//
+//
+
+#ifndef less_functor_h
+#define less_functor_h
+
+#include <string>
+
+
+using namespace std;
+
+namespace CS2312  {             //class CS2312
+
+    template <typename T>
+    class less
+    {
+    public: //return type bool
+    bool operator() (const T &a, const T &b); //argument list
+        {
+            return (a < b);
+        }
+    };
+
+// a functor class can be a _class template_:
+
+
+template <>
+class less <const char *> {
+public:
+    bool operator() (const char *a, const char *b);   //[operator argument list]);
+    {
+        return (a < b);
+
+    }
+
+
+};
+
+
