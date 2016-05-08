362c72d684cbc790cc9b89aa834030fa431adc28
patriciafigueroa <patricia.figueroa@ucdenver.edu>
2016-05-07 19:17:43 -0600

    commit/push : 35/35

--- less_functor.h
+++ less_functor.h
@@ -1,44 +1,56 @@
 //
 //  less_functor.h
-//  PA5
-//
 //  Created by Patricia Figueroa on 5/4/16.
 //
 //
 
-#ifndef less_functor_h
-#define less_functor_h
+#ifndef UCD_CSCI2312_PA5_LESS_FUNCTOR_H
+#define UCD_CSCI2312_PA5_LESS_FUNCTOR_H
 
 #include <string>
 
-
-using namespace std;
-
-namespace CS2312  {             //class CS2312
+namespace CS2312  {             //template less
 
     template <typename T>
     class less
     {
     public: //return type bool
-        bool operator() (const T &a, const T &b); //argument list
+        bool operator() (const T &a, const T &b) //argument list
         {
             return (a < b);
         }
     };
 
-// a functor class can be a _class template_:
+    template<>
+    class less<string> {
+    public:
+        bool operator()(const string &a, const string &b) {
+            return (a < b);
+        }
+    };
 
+    // a functor class can be a _class template_:
+    ///Specialize (char *)
 
     template <>
     class less <const char *> {
     public:
-        bool operator() (const char *a, const char *b);   //[operator argument list]);
+        bool operator() (const char *a, const char *b)   //[operator argument list]);
         {
-            return (a < b);
+            string string_a, string_b;
 
-        }
+            string_a = a;
+            string_b = b;
 
-
+            return ( string_a < string_b );
+        };
     };
+}
+
+#endif //UCD_CSCI2312_PA5_LESS_FUNCTOR_H
+
+
+
+
 
 
